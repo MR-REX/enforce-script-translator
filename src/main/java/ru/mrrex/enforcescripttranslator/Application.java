de6e5e9bc8eab816ea7503f8db82c0c49b212f48
MR.REX <mrrex.main@gmail.com>
2025-03-26 20:12:07 +0300

    Initial commit

---
+++ src/main/java/ru/mrrex/enforcescripttranslator/Application.java
@@ -0,0 +1,21 @@
+package ru.mrrex.enforcescripttranslator;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import ru.mrrex.enforcescripttranslator.command.TranslateCommand;
+
+@Command(
+    name = "Enforce Script Translator",
+    description = "Allows you to translate scripts with custom keywords into Enforce Script according to the specification.",
+    mixinStandardHelpOptions = true,
+    subcommands = {
+        TranslateCommand.class
+    }
+)
+public class Application {
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new Application()).execute(args);
+        System.exit(exitCode);
+    }
+}
---
+++ src/main/java/ru/mrrex/enforcescripttranslator/command/TranslateCommand.java
@@ -0,0 +1,23 @@
+package ru.mrrex.enforcescripttranslator.command;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.concurrent.Callable;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.ExitCode;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "translate",
+        description = "",
+        mixinStandardHelpOptions = true)
+public class TranslateCommand implements Callable<Integer> {
+
+    @Parameters(arity = "1..*", description = "at least one file path to translate",
+            paramLabel = "FILES")
+    private List<Path> filePaths;
+
+    @Override
+    public Integer call() throws Exception {
+        return ExitCode.OK;
+    }
+}
